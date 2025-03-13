e2dbcc3b879ffd1de708561d4b172c5649e7fe25
Andima <andymaking@gmail.com>
2025-03-13 09:44:20 +0100

    F/edge_to_edge

--- android/app/src/main/kotlin/com/prostake/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/prostake/app/MainActivity.kt
@@ -1,7 +1,18 @@
 package com.prostake.app
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import android.os.Bundle
+import androidx.activity.EdgeToEdge
+import androidx.core.view.WindowCompat
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
-    // ...
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Enable edge-to-edge mode using the recommended approach
+        EdgeToEdge.enable(this)
+
+        // Ensure full-screen immersive mode
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
 }
