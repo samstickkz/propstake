5179d7d42edb8fa2a47ff2c55777b44d4ed13cfb
samstickkz <samstickkz@gmail.com>
2025-08-31 08:43:08 +0100

    fixes

--- lib/ui/home/my_investment/faq_details/faq_detail_page.dart
+++ lib/ui/home/my_investment/faq_details/faq_detail_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:propstake/ui/home/profile/profile_home.vm.dart';
